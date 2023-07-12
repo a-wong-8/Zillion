c8f735ed4f197bad24898d0cca0902590d8d578c
a-wong-8 <awong88@berkeley.edu>
2023-07-12 13:55:06 -0700

    fixed marker link

--- frontend/src/components/Map/GeocoderHome.js
+++ frontend/src/components/Map/GeocoderHome.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { Marker, OverlayView } from '@react-google-maps/api';
 import mark from './marker2.png'
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function GeocoderHome({location, price, id}) {
   const [geolocation, setGeolocation] = useState(null)
   const geocoder = new window.google.maps.Geocoder();
+  const history = useHistory();
 
   let address;
 
@@ -22,27 +23,17 @@ export default function GeocoderHome({location, price, id}) {
     }, [address, location]);
 
   const handleClick = () => {
-    window.location.href = `/listings/${id}`;
+    // window.location.href = `/listings/${id}`;
+    history.push(`/listings/${id}`);
   }
 
   return (
-          <>
+      <>
         <Marker
           position={geolocation} label={{color: 'white', text: price}}           
           onClick={handleClick} 
           icon={mark}>
-          {/* <Marker position={geolocation} icon={mark} label={{color: 'white', text: price}}>
-                <OverlayView
-                  position={geolocation}
-                  mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
-                    >
-                  <Link to={`/listings/${id}`}>
-                    <div style={{ color: 'white' }}>{price}</div>  
-                  </Link>
-                </OverlayView>
-              </Marker> */}
-
-          </Marker> 
-          </>
+        </Marker> 
+      </>
     )
 }
