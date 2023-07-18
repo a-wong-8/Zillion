1a4071b3efa4f18fabc83c7ecdab458515024305
a-wong-8 <awong88@berkeley.edu>
2023-07-17 18:26:27 -0700

    attempt to fix refresh error

--- app/controllers/api/saves_controller.rb
+++ app/controllers/api/saves_controller.rb
@@ -3,8 +3,12 @@ class Api::SavesController < ApplicationController
     wrap_parameters include: Save.attribute_names + ['listingId', 'userId']
 
     def index
-        @listings = current_user.saved_listings
-        render "api/listings/index"
+        if !current_user.nil?
+            @listings = current_user.saved_listings
+            render "api/listings/index"
+        else 
+            render json: @listings.errors.full_messages, status: 422
+        end
         # @saves = Save.all
         # render :index
     end
@@ -28,7 +32,6 @@ class Api::SavesController < ApplicationController
 
         @save = Save.find_by(listing_id: listingId, user_id: userId)
         @save.destroy
-        # head :no_content
         render json: ['successfully unsaved']
     end
 
